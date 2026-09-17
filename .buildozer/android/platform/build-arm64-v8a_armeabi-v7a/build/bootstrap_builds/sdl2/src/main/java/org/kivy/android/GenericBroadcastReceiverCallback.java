c4b465703c143e8218f2665a95ccff8b572fa617
Yoshun Kimura <yoshik@YoshunnoMacBook-Air.local>
2026-09-18 01:35:47 +0900

    add ndk download retry, ignore .buildozer generated files

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/src/main/java/org/kivy/android/GenericBroadcastReceiverCallback.java
+++
@@ -1,9 +0,0 @@
-package org.kivy.android;
-
-import android.content.Context;
-import android.content.Intent;
-
-public interface GenericBroadcastReceiverCallback {
-    void onReceive(Context context, Intent intent);
-}
-;
